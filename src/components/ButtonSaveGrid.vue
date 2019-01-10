e2aece2a9035ed51d9eccb9d68019e6ee490b95c
Benjamin <benjipoi@hotmail.com>
2019-01-10 16:51:35 +0100

    first commit

---
+++ src/components/ButtonSaveGrid.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <button @click="clickToJson" type="button">{{ msg }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ButtonSaveGrid',
+  props: {
+    msg: String,
+    layout: Array
+  },
+    methods: {
+        clickToJson: function() {
+            function keepIXYWHentry(obj) {
+                var temp = {};
+                temp.i = obj.i;
+                temp.w = obj.w;
+                temp.x = obj.x;
+                temp.y = obj.y;
+                temp.h = obj.h;
+
+                return temp;
+            }
+
+            var out = this.layout.map(keepIXYWHentry);
+
+            console.log(JSON.stringify(out))
+        }
+    }
+}
+</script>
+
+<style scoped>
+button {
+  margin: 40px;
+}
+</style>
